fbeefe2916c84f2593174e0bb7160ee21aae9a38
EdwardBelt <beltranmarper@gmail.com>
2026-03-04 18:47:32 +0100

    fix: invalid constructor

--- core/src/main/java/es/edwardbelt/hycraft/network/handler/minecraft/data/nbt/NbtBoolean.java
+++ core/src/main/java/es/edwardbelt/hycraft/network/handler/minecraft/data/nbt/NbtBoolean.java
@@ -1,23 +1,19 @@
 package es.edwardbelt.hycraft.network.handler.minecraft.data.nbt;
 
-import es.edwardbelt.hycraft.protocol.io.PacketBuffer;
 import lombok.Getter;
-
-import java.nio.charset.StandardCharsets;
 
 @Getter
 public class NbtBoolean extends NbtByte {
 
     public NbtBoolean(boolean value, String name) {
-        super(name);
-        this.value = getByteByBool(value);
+        super(getByteByBool(value), name);
     }
 
     public NbtBoolean(boolean value) {
         this(value, null);
     }
 
-    private byte getByteByBool(boolean value) {
-        return value ? 1 : 0;
+    private static byte getByteByBool(boolean value) {
+        return value ? (byte) 1 : 0;
     }
 }
